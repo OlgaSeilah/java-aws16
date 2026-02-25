1d8fb0d3c6b908932a673c963f4e2fb66998c8b4
seilah <princenoru@gmail.com>
2026-02-25 15:04:24 +0100

    hw_2

--- src/CountNumbers.java
+++ src/CountNumbers.java
@@ -1,14 +1,15 @@
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.TreeMap;
 
 public class CountNumbers {
     public static void main(String[] args) {
         Random r = new Random();
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new TreeMap<>();
 
         for (int i = 0; i < 100; i++) {
-            int randomNumber = r.nextInt(10);
+            int randomNumber = r.nextInt(10)+1;
 
             if (!map.containsKey(randomNumber)) {
                 map.put(randomNumber, 0);
